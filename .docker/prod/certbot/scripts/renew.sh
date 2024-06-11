1ff2c8bb267291638921cbacced5409485031da6
Андрей <parabellum.koval@gmail.com>
2024-06-11 13:08:01 +0300

    docker ssl

--- .docker/prod/certbot/scripts/renew.sh
+++ .docker/prod/certbot/scripts/renew.sh
@@ -4,7 +4,9 @@ certbot renew \
     --non-interactive \
     --webroot \
     --webroot-path /var/www/acme \
-    --post-hook /scripts/nginx-reload.sh
-    # --force-renewal \
-    # --no-random-sleep-on-renew \
-    # --staging
+    --post-hook /scripts/nginx-reload.sh \
+    --cert-name djini.com.ua \
+    --force-renewal \
+    --no-random-sleep-on-renew \
+    --staging
+    # --config-dir /etc/nginx/ssl
