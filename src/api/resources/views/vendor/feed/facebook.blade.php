d86b21a3a04d9b6773c05eb50cc01ff253386f31
Андрей Коваль <parabellum.koval@gmail.com>
2025-07-16 11:53:20 +0300

    xml namespace

--- src/api/resources/views/vendor/feed/facebook.blade.php
+++ src/api/resources/views/vendor/feed/facebook.blade.php
@@ -2,7 +2,7 @@
 /* Using an echo tag here so the `<? ... ?>` won't get parsed as short tags */
 '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
 ?>
-<rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
+<rss version="2.0">
   <channel>
     <title><![CDATA[{{ $meta['title'] }}]]></title>
     <link><![CDATA[{{ url($meta['link']) }}]]></link>
