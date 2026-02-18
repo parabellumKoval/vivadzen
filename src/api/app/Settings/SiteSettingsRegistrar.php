4253bd345df216e05fac5968b8dbb7fc2158dbe7
Андрей Коваль <parabellum.koval@gmail.com>
2026-02-18 13:38:36 +0200

    reviews

--- src/api/app/Settings/SiteSettingsRegistrar.php
+++ src/api/app/Settings/SiteSettingsRegistrar.php
@@ -54,37 +54,37 @@ class SiteSettingsRegistrar implements SettingsRegistrarInterface
                         ->label('Viber')
                         ->cast('string')
                         ->regionable(true)
-                        ->tab('Социальные сети и мессенджеры')
+                        ->tab('Мессенджеры')
                     );
                     $page->add(Field::make('site.contacts.social.whatsapp', 'text')
                         ->label('Whatsapp')
                         ->cast('string')
                         ->regionable(true)
-                        ->tab('Социальные сети и мессенджеры')
+                        ->tab('Мессенджеры')
                     );
                     $page->add(Field::make('site.contacts.social.telegram', 'text')
                         ->label('Telegram')
                         ->cast('string')
                         ->regionable(true)
-                        ->tab('Социальные сети и мессенджеры')
+                        ->tab('Мессенджеры')
                     );
                     $page->add(Field::make('site.contacts.social.instagram', 'text')
                         ->label('Instagram')
                         ->cast('string')
                         ->regionable(true)
-                        ->tab('Социальные сети и мессенджеры')
+                        ->tab('Социальные сети')
                     );
                     $page->add(Field::make('site.contacts.social.facebook', 'text')
                         ->label('Facebook')
                         ->cast('string')
                         ->regionable(true)
-                        ->tab('Социальные сети и мессенджеры')
+                        ->tab('Социальные сети')
                     );
                     $page->add(Field::make('site.contacts.social.youtube', 'text')
                         ->label('Youtube')
                         ->cast('string')
                         ->regionable(true)
-                        ->tab('Социальные сети и мессенджеры')
+                        ->tab('Социальные сети')
                     );
                 })
 
